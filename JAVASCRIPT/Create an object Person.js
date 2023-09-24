7e25ac56bb2a2948c91ffafdbe88a60d7c766563
SiddharthBahuguna <112819453+SiddharthBahuguna@users.noreply.github.com>
2023-09-24 21:42:24 +0530

    Add files via upload

---
+++ JAVASCRIPT/Create an object Person.js
@@ -0,0 +1,8 @@
+const Person = {
+    name: "Jane Doe",
+    age: 21,
+    city: "New Jersey",
+    };
+    Person.city = "New York";
+    Person.country = "United States";
+    console.log(Person);
